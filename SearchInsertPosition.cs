89f5bc5ce8a23a3152eb7e3acf333bcc6aecda0e
leescholefield <leescholefield@gmail.com>
2020-01-29 13:13:21 +0000

    Removed redundant checks in SearchInsertPosition
    Instead of checking nums[mid + 1] and nums[mid -1] we can simply return low; if low and high are equal and it breaks out of the while loop the last low value must be the only possible index position target could be.

--- SearchInsertPosition.cs
+++ SearchInsertPosition.cs
@@ -7,7 +7,7 @@
 // You may assume no duplicates in the array.
 public class Solution {
 
-    public int SearchInsert(int[] nums, int target) {
+     public int SearchInsert(int[] nums, int target) {
         
         int high = nums.Length - 1;
         int low = 0;
@@ -15,34 +15,18 @@ public class Solution {
         while (low <= high) {
             int mid = (high + low) / 2;
             int guess = nums[mid];
-
+            
             if (guess == target) {
                 return mid;
             }
             if (guess < target) {
-                if (mid == nums.Length - 1) {
-                    return nums.Length;
-                }
-                
-                if (nums[mid + 1] > target) {
-                    return mid + 1;
-                }
-                
                 low = mid + 1;
             }
             else {
-                if (mid == 0) {
-                    return 0;
-                }
-                
-                if (nums[mid - 1] < target) {
-                    return mid;
-                }
-                
                 high = mid - 1;
             }
         }
         
-        return 0;
+        return low;
     }
 }
