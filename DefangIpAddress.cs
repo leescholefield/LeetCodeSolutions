7fdac9489e56fb5280c51dec38f5ca78e80eac1a
leescholefield <leescholefield@gmail.com>
2020-02-02 16:11:51 +0000

    Create DefangIpAddress.cs

---
+++ DefangIpAddress.cs
@@ -0,0 +1,18 @@
+// Runtime 76ms -- faster than 96.61%
+//
+// Given a valid (IPv4) IP address, return a defanged version of that IP address.
+// A defanged IP address replaces every period "." with "[.]".
+//
+public class Solution {
+    public string DefangIPaddr(string address) {
+        
+        for (int i = address.Length - 1; i > 0; i--) {
+            if (address[i] == '.') {
+                address = address.Substring(0, i) + "[.]" + address.Substring(i+1); 
+            }
+        }
+        
+        return address;
+        
+    }
+}
